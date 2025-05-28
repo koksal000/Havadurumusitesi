e4aed2f5f114bf85d959d1d1332b9490e60dcfe2
Domaniç Çamlıca Köyü <domaniccamlicakoyu@gmail.com>
2025-05-28 10:37:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Attempted to call useSound() from the server but useSound is on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.. Error source: src/components/ui/button.tsx (46:40) @ Button
    >
    >   44 |   ({ className, variant, size, asChild = false, onClick, ...props }, ref) => {
    >   45 |     const Comp = asChild ? Slot : "button"
    > > 46 |     const { playClickSound } = useSound();
    >      |                                        ^
    >   47 |
    >   48 |     const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
    >   49 |       playClickSound();
    >
    > Call Stack
    > 6
    >
    > Show 4 ignore-listed frame(s)
    > <anonymous>
    > rsc:/Server/file:///home/user/studio/.next/server/chunks/ssr/%5Broot%20of%20the%20server%5D__fbf375e5._.js (457:11)
    > Button
    > src/components/ui/button.tsx (46:40)

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,10 @@
+
+'use client'; // Added 'use client' directive
+
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-import { useSound } from "@/contexts/SoundContext"; // Added useSound import
+import { useSound } from "@/contexts/SoundContext"; 
 
 import { cn } from "@/lib/utils"
 
